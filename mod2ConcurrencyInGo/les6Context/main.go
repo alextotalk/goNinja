235185252aad9d87b47d0d8c94c8ca50f3f12f7b
Заяц Алексей <alexoo@inbox.ru>
2021-11-05 00:16:49 +0200

    Mod2 Les6 Context

--- mod2ConcurrencyInGo/les6Context/main.go
+++ mod2ConcurrencyInGo/les6Context/main.go
@@ -2,10 +2,41 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"time"
 )
 
 func main() {
+	t := time.Now()
+	defer func() { fmt.Println(time.Since(t)) }()
 	ctx := context.Background()
-	ctx, _ = context.WithTimeout(ctx, time.Second)
+
+	ctx = context.WithValue(ctx, "id", 1)
+
+	//context.TODO()
+	//ctx, cancel := context.WithTimeout(ctx, time.Second*3)
+	//
+	//go func() {
+	//	time.Sleep(time.Millisecond * 100)
+	//	cancel()
+	//}()
+
+	pars(ctx)
+}
+
+func pars(ctx context.Context) {
+	id := ctx.Value("id")
+	//context.WithDeadline(ctx,time.Now())
+	fmt.Println(id)
+	for {
+		select {
+		case <-time.After(time.Second * 2):
+			fmt.Println("parsing completed")
+			return
+		case <-ctx.Done():
+			fmt.Println("deadline exceeded on select")
+			return
+		}
+
+	}
 }
